7adb9465a5b95d5584b28a628d9246a10c27edb9
Dhruv Gupta <dhruv06012@gmail.com>
2025-02-21 18:38:37 +0530

    srrw gfrwy

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -40,12 +40,13 @@ const HeroSection = () => {
             I love crafting innovative solutions to real-world problems.
           </p>
           <div>
-            <Link
-              href="mailto:dhruv06012@gmail.com"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
-            >
-              Hire Me
-            </Link>
+          <a
+  href="mailto:dhruv06012@gmail.com"
+  className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+>
+  Hire Me
+</a>
+
             <a
     href="/Morgan Housel.pdf"
     download="Morgan Housel.pdf"
